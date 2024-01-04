18a6fc05f179655c67250f81e6757253893cb48f
lanzejun <lanzejun@cn.ibm.com>
2024-01-04 13:31:13 +0800

    update

--- app/(platform)/(dashboard)/user/page.tsx
+++ app/(platform)/(dashboard)/user/page.tsx
@@ -1,17 +1,65 @@
-import { Flex, Text, Button } from '@radix-ui/themes';
-import { Metadata } from 'next';
+import { promises as fs } from "fs"
+import path from "path"
+import { Metadata } from "next"
+import Image from "next/image"
+import { z } from "zod"
+
+import { columns } from "./_components/columns"
+import { DataTable } from "./_components/data-table"
+import { UserNav } from "./_components/user-nav"
+import { taskSchema } from "./data/schema"
 
 export const metadata: Metadata = {
-  title: 'User'
-};
+  title: "Tasks",
+  description: "A task and issue tracker build using Tanstack Table.",
+}
 
-export default function UserPage() {
+// Simulate a database read for tasks.
+async function getTasks() {
+  const data = await fs.readFile(
+    path.join(process.cwd(), "app/(platform)/(dashboard)/user/data/tasks.json")
+  )
+
+  const tasks = JSON.parse(data.toString())
+
+  return z.array(taskSchema).parse(tasks)
+}
+
+export default async function TaskPage() {
+  const tasks = await getTasks()
+
   return (
-    <main className="flex justify-center md:h-screen">
-      <Flex direction="column" gap="2">
-        <Text>Hello from Radix Themes :)</Text>
-        <Button>Let's go</Button>
-      </Flex>
-    </main>
-  );
+    <>
+      <div className="md:hidden">
+        <Image
+          src="/examples/tasks-light.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="block dark:hidden"
+        />
+        <Image
+          src="/examples/tasks-dark.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="hidden dark:block"
+        />
+      </div>
+      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+            <p className="text-muted-foreground">
+              Here&apos;s a list of your tasks for this month!
+            </p>
+          </div>
+          <div className="flex items-center space-x-2">
+            <UserNav />
+          </div>
+        </div>
+        <DataTable data={tasks} columns={columns} />
+      </div>
+    </>
+  )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import '@radix-ui/themes/styles.css';
-import { Theme } from '@radix-ui/themes';
+import { Theme, ThemePanel } from '@radix-ui/themes';
 
 import { siteConfig } from '@/config/site'
 
@@ -33,8 +33,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Theme accentColor="crimson" grayColor="sand" radius="large" scaling="95%">
+        <Theme appearance="light" grayColor="sand" radius="large" scaling="95%">
           {children}
+          {/* <ThemePanel /> */}
         </Theme>
       </body>
     </html>
